a15ba0c6dfb9eca50e896c56928bc6b3c25b0e72
Krushna1621 <shindekrushna825@gmail.com>
2023-09-30 19:53:25 +0530

    adding singup form

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,13 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+
+export const useWorkoutsContext=()=>{
+    const context=useContext(AuthContext)
+
+    if(!context){
+        throw Error('useWorkoutsContext must be used inside an AuthContextProvider')
+    }
+
+    return context
+}
